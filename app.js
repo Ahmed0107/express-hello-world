2beb130a7be82a18107508ffcdb7299135bcd1a7
Ahmed0107 <65514906+Ahmed0107@users.noreply.github.com>
2022-10-02 01:00:12 +0200

    add route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/api", (req, res) => {
+res.send([{"id":1, "name":"ahmed"}])
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
